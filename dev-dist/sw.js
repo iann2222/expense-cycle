d0ca08287200a056083ccdcd52b42f302e48719b
iann2222 <e04ian@gmail.com>
2026-07-11 14:23:49 +0800

    更新 README.md

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-7a5e81cd'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "/index.html",
-    "revision": "0.5dd345jkm2c"
+    "revision": "0.33dos4nefoc"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
